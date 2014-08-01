3adea4258600fe8bde2e2897ebb17528ad07154c
jamesrtyler <jrt341@gmail.com>
2014-08-01 15:51:02 -0500

    Add a visuallyhidden class to wp_head.
    Added visuallyhidden class  to head using wp_head action. This class is being used to make the hiding of the label accessible for screen readers and other assistive devices.

--- gravityforms-placeholders.php
+++ gravityforms-placeholders.php
@@ -22,3 +22,20 @@ function gf_placeholder_enqueue_scripts() {
 	echo "<script>var jquery_placeholder_url = '" . $plugin_url . "/jquery.placeholder-1.0.1.js';</script>";
 	wp_enqueue_script('_gf_placeholders', $plugin_url . '/gf.placeholders.js', array('jquery'), '1.0' );
 }
+
+/* Adds visuallyhidden class to the head. Theme must be using wp_head for this to work. If your theme is missing wp_head, please copy the style below and add it to your main stylesheet.*/
+add_action('wp_head','hook_gf_placeholder_css');
+function hook_gf_placeholder_css()
+{
+$output="<style> 
+		.visuallyhidden { 
+		position: absolute; 
+		overflow: hidden; 
+		clip: rect(0 0 0 0); 
+		height: 1px; width: 1px; 
+		margin: -1px; padding: 0; border: 0; 
+		}
+	</style>";
+
+echo $output;
+}
